0468704b4ac4b27dbfc91649a33a3d00fe5be4a6
Sebastian Ramirez <ramirezsebastian72@gmail.com>
2024-05-29 18:55:35 -0500

    first commit - initialized project

---
+++ app/src/main/java/com/juanRamirez/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.juanRamirez.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.juanRamirez.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
